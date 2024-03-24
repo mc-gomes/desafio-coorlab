62a00af76a666de2384fb6e420cc9682193e123e
mc-gomes <bigmath2001@hotmail.com>
2024-03-24 17:43:31 -0300

    implementing first routes

--- app/backend/app.py
+++ app/backend/app.py
@@ -11,5 +11,24 @@ CORS(app)
 def hello():
     return 'Hello from Flask!'
 
-app.run(port=3000)
+@app.route('/destinations', methods=['GET'])
+def get_destinations():
+    return jsonify(data)
+
+@app.route('/destinations/<int:id>', methods=['GET'])
+def get_destination_by_id(id: int):
+    destination = get_destination(id)
+    if destination is None:
+        return jsonify({'error': 'Nenhum dado encontrado'}, 404)
+    return jsonify(destination)
+
+def get_destination(id):
+    if 0 < id < len(data) and data[id]:
+        return data[id-1]
+    else:
+        return None
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=3000)
 
