b6e8f0896d34f07d4caeaee7573733f0f5b5701f
mc-gomes <bigmath2001@hotmail.com>
2024-03-25 02:38:01 -0300

    script for run.sh

--- app/run.sh
+++ app/run.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 
-echo "Implemente aqui o script para executar a sua solução"
+# instalação de dependências para o frontend
+cd frontend
+
+npm install -g @vue/cli
+npm install axios
+
+# instalação de dependências para o backend
+cd ../backend
+pip install Flask
+pip install -U flask-cors
+
+
+# executando a aplicação
+
+# rodando servidor do frontend 
+# (comando abaixo sendo executado em segundo plano)
+npm run serve &
+
+# rodando servidor backend (API)
+cd ../backend
+python3 app.py
+
