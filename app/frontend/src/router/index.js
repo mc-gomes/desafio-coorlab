7906a8403dd0ace83d0c06c1921309458009a5a1
mc-gomes <bigmath2001@hotmail.com>
2024-03-24 23:32:43 -0300

    delete unused route

--- app/frontend/src/router/index.js
+++ app/frontend/src/router/index.js
@@ -1,5 +1,4 @@
 import { createRouter, createWebHashHistory } from "vue-router";
-// import HomeView from "../views/HomeView.vue";
 import MainPageView from "../views/MainPageView.vue";
 
 const routes = [
@@ -7,15 +6,6 @@ const routes = [
     path: "/",
     name: "home",
     component: MainPageView,
-  },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
   },
 ];
 
